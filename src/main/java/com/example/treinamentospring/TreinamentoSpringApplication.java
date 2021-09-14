23d2e8af0954d12bcbc9e720a3d06e7ebb0e159f
Diego Matté <diegocmatte@gmail.com>
2021-09-13 23:49:30 -0300

    criando projeto

---
+++ src/main/java/com/example/treinamentospring/TreinamentoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.treinamentospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/treinamentospring/TreinamentoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.treinamentospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
